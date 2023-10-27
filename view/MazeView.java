28ed2ef04d5c999903f23ad1bc22166bb5bad550
d4hy <davidhoang253@gmail.com>
2023-10-27 13:58:09 -0700

    Added a JFrame to hold the maze view in the trivia maze class.

--- view/MazeView.java
+++ view/MazeView.java
@@ -1,4 +1,63 @@
 package view;
 
-public class MazeView {
+
+
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+/**
+ *  This class serves as a screen class that displays the game.
+ * @author David Hoang, Faith Capito.
+ * @version Fall 2023
+ */
+public class MazeView extends JPanel {
+
+    //Screen settings.
+    /**
+     *  16x 16 tiles, will represent size of characters,and parts of the map.
+     */
+    private final int myOriginalTileSize = 16;
+    /**
+     *  This variable will be used to upscale the tile size otherwise it's too small to see.
+     */
+    private final int myScale = 3;
+
+    /**
+     * The tile size up-scaled to fit on the screen.
+     */
+    private final int myTileSize = myOriginalTileSize * myScale;
+
+    /**
+     *The amount of tiles that will be represented as the rows of the screen.
+     *
+     */
+    private final int myMaxScreenRow = 16;
+    /**
+     *The amount of tiles that will be represented as the columns of the screen.
+     *
+     */
+    private final int myMaxScreenCol = 12;
+
+    /**
+     * The up-scaled amount of tiles that will be represented as the width of the screen.
+     */
+    private final int myScreenWidth = myTileSize * myMaxScreenRow; //768 pixels.
+
+    /**
+     * The up-scaled amount of tiles that will be represented as the height of the screen.
+     */
+    private final int myScreenHeight = myTileSize * myMaxScreenRow; // 576 pixels.
+
+    MazeView() {
+        //sets the size of the jPanel.
+        this.setPreferredSize(new Dimension(myScreenWidth, myScreenHeight));
+        this.setBackground(Color.black);
+
+        //will improve game's rendering performance.
+        this.setDoubleBuffered(true);
+    }
+
 }
--- view/TriviaMaze.java
+++ view/TriviaMaze.java
@@ -1,4 +1,47 @@
+
 package view;
 
-public class TriviaMaze {
+import javax.swing.JFrame;
+
+/**
+ * This class will serve as the container class where everything will be displayed and ran.
+ * @author David Hoang, Faith Capito
+ * @version Fall 2023
+ */
+public  final class TriviaMaze {
+
+    /**
+     * Static variable used to contain the gui components.
+     */
+    private static JFrame myWindow;
+
+     // So there can't be a constructor for this driver class.
+    private TriviaMaze() {
+        throw new UnsupportedOperationException();
+    }
+    public static void main(final String[] theArgs) {
+        myWindow = new JFrame();
+        setUp();
+        addMazeView();
+    }
+
+    private static void setUp() {
+
+        //This lets the window properly close when user clicks X button.
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myWindow.setResizable(false);
+        myWindow.setTitle("Beabadobee Trivia Maze");
+        //Doesn't specify the location of the window, since it is null
+        // it wil be displayed in the center.
+        myWindow.setLocationRelativeTo(null);
+        myWindow.setVisible(true);
+    }
+    private static void addMazeView() {
+        final MazeView mazeview = new MazeView();
+        myWindow.add(mazeview);
+        //Window will be sized to fit the preferred size
+        // and layouts of its subcomponents.
+        myWindow.pack();
+
+    }
 }
